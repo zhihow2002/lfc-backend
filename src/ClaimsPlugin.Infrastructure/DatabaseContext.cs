66a237fd2204d368901246d5caf708dca888b806
zhihow <37094488+zhihow2002@users.noreply.github.com>
2024-04-04 22:04:08 +0800

    add DbContext

--- src/ClaimsPlugin.Infrastructure/DatabaseContext.cs
+++ src/ClaimsPlugin.Infrastructure/DatabaseContext.cs
@@ -5,23 +5,32 @@ namespace ClaimsPlugin.Infrastructure
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options)
-            : base(options)
-        {
-            Users = Set<User>();
-            Tokens = Set<TokenManager>();
-        }
+        public DatabaseContext() { }
 
-        public DbSet<User>? Users { get; set; }
-        public DbSet<TokenManager> Tokens { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options) { }
+
+        public DbSet<User>? Users => Set<User>();
+        public DbSet<TokenManager> Tokens => Set<TokenManager>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(i =>
+            {
+                i.ToTable("Users");
+                i.HasKey(k => k.Id);
+            });
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                string mySqlConnectionStr =
+                const string ConnectionStr =
                     "Data Source=ZHIHOW;Initial Catalog=LFC;Integrated Security=True;TrustServerCertificate=True;MultipleActiveResultSets=True";
-                optionsBuilder.UseSqlServer(mySqlConnectionStr);
+                optionsBuilder.UseSqlServer(ConnectionStr);
             }
         }
     }
